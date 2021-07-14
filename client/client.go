ee2e3b93eb59fd4abe13027c40ada1dcb32cdaff
HuangJunwei <825346351@qq.com>
2021-07-15 00:58:24 +0800

    'fix'

--- client/client.go
+++ client/client.go
@@ -13,8 +13,8 @@ const (
 )
 
 func main() {
-	ip, port := "localhost", "8080"
-	//ip, port := "123.57.29.103", "8080"
+	//ip, port := "localhost", "8080"
+	ip, port := "123.57.29.103", "8080"
 	host := ip + ":" + port
 	fmt.Println("lite-chat start...")
 	conn, err := net.Dial("tcp", host)
